711afe40d80d10d6a8444e0c56b6561f469bb78a
luigigiuliani17 <luigigiuliani17@gmail.com>
2015-12-24 15:51:41 +0100

    - add fake recommender class (black box not implemented!!!)

---
+++ GlamourLights/GlamourLights/GlamourLights/Controller/Recommender.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GlamourLights.Model;
+
+namespace GlamourLights.Controller
+{
+    /// <summary>
+    /// BLACK BOX; WILL NOT BE IMPLEMENTED!!!!!!!!!
+    /// </summary>
+    class Recommender
+    {
+        /// <summary>
+        /// BLACK BOX; WILL NOT BE IMPLEMENTED!!!!!!!!!
+        /// </summary>
+        /// <param name="cust"></param> customer that needs a recommendation
+        /// <returns></returns> a list of recommendations
+        public item[] getPersonalizedRecommendations(customer cust)
+        {
+
+            return null;
+        }
+
+
+
+        /// <summary>
+        /// BLACK BOX; WILL NOT BE IMPLEMENTED!!!!!!!!!
+        /// </summary>
+        /// <returns></returns> some not personalized recommendations
+        public item[] getNotPersonalizedRecommendations()
+        {
+            return null;
+        }
+    }
+}
