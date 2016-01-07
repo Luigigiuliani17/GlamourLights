ff13582966eb227e4d4cccd49e91574ac3b4b9fa
luigigiuliani17 <luigigiuliani17@gmail.com>
2016-01-07 15:37:02 +0100

    added matrix button class for the admin personalization part

---
+++ GlamourLights/GlamourLights/GlamourLights/Model/matrixButton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace GlamourLights.Model
+{
+    public class matrixButton : Button
+    {
+        public int x_cord { get; set; }
+        public int y_cord { get; set; }
+
+        public matrixButton (int x, int y)
+        {
+
+            this.x_cord = x_cord;
+            this.y_cord = y_cord;
+        }
+    }
+}
